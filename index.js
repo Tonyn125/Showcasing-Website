2b3fb133ebc8ca805e9729bb65bb3b0a42939331
Tonyn125 <tonyn1@student.unimelb.edu.au>
2017-11-20 20:33:54 +1100

    Structure and Bootstrap : Add Personal CSS AFTER Other CSS

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ var app = express();
 var path = require('path');
 app.use(express.static('public'));
 
-app.get('/', function(req, res){
-  res.sendFile(path.join(__dirname+'public/index.html'));
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + 'public/index.html'));
 
 });
 
 
-app.listen(3000, function(req, res){
-  console.log('Listening on port 3000')
+app.listen(3000, function(req, res) {
+  console.log('Listening on port 3000');
   //net stop http
 });
